48dd0d729c1cb7cc525ab9aef320c7fc4fa5dc90
Hary Miarina <ratsihoaranavoaharymiarina@gmail.com>
2025-06-29 22:57:45 +0300

    em

--- Pages/employes.php
+++ Pages/employes.php
@@ -2,8 +2,6 @@
     require("../Inclus/fonctions.php");
     $idDepart = $_GET['id'];
     $employees = getEmployeesParDepartParId($idDepart);
-    $count = 1;
-    $next = getNextEmployees($nomEmployer,$departName,$ageMin,$ageMax,$count);
 ?>
 <!DOCTYPE html>
 <html lang="en">
