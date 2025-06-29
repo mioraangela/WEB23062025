8a5de21888b14e6908f208208708205f7a0f5d43
Hary Miarina <ratsihoaranavoaharymiarina@gmail.com>
2025-06-29 16:48:14 +0300

    getEmployerParId

--- Inclus/fonctions.php
+++ Inclus/fonctions.php
@@ -31,7 +31,7 @@
         $sql = "SELECT departments.dept_no, departments.dept_name
         , employees.birth_date,employees.first_name, 
         employees.last_name, employees.gender, 
-        employees.hire_date
+        employees.emp_no,employees.hire_date
         FROM departments
         Join dept_emp
         on departments.dept_no = dept_emp.dept_no
@@ -47,10 +47,26 @@
         return $result;
     }
 
+    function getEmployeesParId($idEmploye){
+        $bdd = dbconnect();
+        $sql = "SELECT * 
+        FROM employees
+        Where emp_no ='%s'
+        ";
+        $sql = sprintf($sql, $idEmploye);
+        $req = mysqli_query($bdd,$sql );
+        $result = array();
+        while ($news = mysqli_fetch_assoc($req)) {
+            $result[] = $news;
+        }
+        mysqli_free_result($req);
+        return $result;
+    }
+
     function getEmployeesParDepartParId($idDepart){
         $bdd = dbconnect();
         $sql = "SELECT departments.dept_no, departments.dept_name
-        , employees.birth_date,employees.first_name, 
+        , employees.birth_date,employees.first_name,employees.emp_no, 
         employees.last_name, employees.gender,
         employees.hire_date
         FROM departments
