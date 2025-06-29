663ae3931a270f9c21387937c13a77654b9acdfd
Hary Miarina <ratsihoaranavoaharymiarina@gmail.com>
2025-06-29 13:01:23 +0300

    function getManager

--- Inclus/fonctions.php
+++ Inclus/fonctions.php
@@ -40,5 +40,26 @@
         return $result;
     }
 
+    function getCurrentManager(){
+        $bdd = dbconnect();
+        $sql = "SELECT *
+            FROM employees e
+            Join dept_manager dm
+            on e.emp_no = dm.emp_no
+            Join departments d
+            on d.dept_no = dm.dept_no
+            WHERE dm.to_date ='9999-01-01';
+        ";
+        $req = mysqli_query($bdd,$sql);
+        $result = array();
+        while ($news = mysqli_fetch_assoc($req)) {
+            $result[] = $news;
+        }
+        mysqli_free_result($req);
+        return $result;
+    }
+
+
+
     
 ?>
