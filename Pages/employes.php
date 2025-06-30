ec47ecf91a49e77e4c9c3d1caff46d5b659c9b40
Hary Miarina <ratsihoaranavoaharymiarina@gmail.com>
2025-06-30 22:12:52 +0300

    employes modif

--- Pages/employes.php
+++ Pages/employes.php
@@ -1,7 +1,39 @@
 <?php 
     require("../Inclus/fonctions.php");
-    $idDepart = $_GET['id'];
-    $employees = getEmployeesParDepartParId($idDepart);
+    if(isset($_GET['idDepart'])){
+        $idDepart = $_GET['idDepart'];
+    }
+    else{
+        $idDepart = $_GET['id'];
+    }
+    
+    $totalTab= getTotalEmployeesParDepartParId($idDepart);
+    $totalEmployees = $totalTab['count'];
+    $itemPerPage = 20;
+    
+    if(isset($_GET['page'])){
+        $page = $_GET['page'];
+    }
+    else{
+        $page = 1;
+    }
+
+    if(isset($_GET['totalPage'])){
+        $totalPages = $_GET['totalPage'];
+    }
+    else{
+        $totalPages = ceil($totalEmployees/$itemPerPage);
+    }
+    if($page <= 0){
+        $page = 1;
+        header("location: ?page=$page&&totalPage=$totalPages&&idDepart=$idDepart");
+    }
+    if($page > $totalPages){
+        $page = $totalPages;
+        header("location: ?page=$page&&totalPage=$totalPages&&idDepart=$idDepart");
+    }
+    $offset = ($page - 1)*$itemPerPage;
+    $employees = getEmployeesParDepartParId($idDepart,$offset);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -42,6 +74,58 @@
 
             <?php } ?>
         </table>
+        <div>
+            
+            <?php if($page == $totalPages){?>
+                <span>
+                    <a href="?page=<?=$page - 4?>&&totalPage=<?=$totalPages?>&&idDepart=<?=$idDepart?>"><< </a>
+                    <a href="?page=<?=$page?>&&totalPage=<?=$totalPages?>&&idDepart=<?=$idDepart?>"> <?=$page?></a>
+                </span>
+
+            <?php } else if($page == $totalPages - 4){ ?>
+                <a href="?page=<?=$page - 1?>&&totalPage=<?=$totalPages?>&&idDepart=<?=$idDepart?>"><<</a>
+                <span>
+                    <?php for($i = 0; $i < 3;$i++){?>
+                        <a href="?page=<?= $page + $i + 1?>&&totalPage=<?=$totalPages?>&&idDepart=<?=$idDepart?>"><?= $page + $i + 1?></a>
+                    <?php }?>
+                </span>
+                <span>. . . . .</span>
+                <a href="?page=<?=$totalPages?>&&totalPage=<?=$totalPages?>&&idDepart=<?=$idDepart?>"><?=$totalPages?></a>
+            
+            
+            <?php } else if($page == 1) { ?>
+                <span>
+                    <?php for($i = 0; $i < 3;$i++){?>
+                        <a href="?page=<?= $page + $i + 1?>&&totalPage=<?=$totalPages?>&&idDepart=<?=$idDepart?>"><?= $page + $i + 1?></a>
+                    <?php }?>
+                </span>
+                <span>. . . . .</span>
+                <a href="?page=<?=$totalPages?>&&totalPage=<?=$totalPages?>&&idDepart=<?=$idDepart?>"><?=$totalPages?></a>
+                <a href="?page=<?=$page + 1?>&&totalPage=<?=$totalPages?>&&idDepart=<?=$idDepart?>"> >></a>   
+
+            <?php } else { ?>
+                <a href="?page=<?=$page - 1?>&&totalPage=<?=$totalPages?>&&idDepart=<?=$idDepart?>"><<</a>
+                <span>
+                    <?php for($i = 0; $i < 3;$i++){?>
+                        <a href="?page=<?= $page + $i + 1?>&&totalPage=<?=$totalPages?>&&idDepart=<?=$idDepart?>"><?= $page + $i + 1?></a>
+                    <?php }?>
+                </span>
+                <span>. . . . .</span>
+                <a href="?page=<?=$totalPages?>&&totalPage=<?=$totalPages?>&&idDepart=<?=$idDepart?>"><?=$totalPages?></a>
+                <a href="?page=<?=$page + 1?>&&totalPage=<?=$totalPages?>&&idDepart=<?=$idDepart?>"> >></a>
+            <?php } ?>
+        </div>
+        <div>
+            <form action="" method="get">
+                <p>Naviguer vers la page 
+                    <input type="number" name="page" id="page" required>
+                </p>
+                <input type="hidden" name="totalPage" value="<?= $totalPages ?>">
+                <input type="hidden" name="idDepart" value="<?= $idDepart ?>">
+                <input type="submit" value="Valider">
+            </form>
+        </div>
+
     </main>
 </body>
 </html>
